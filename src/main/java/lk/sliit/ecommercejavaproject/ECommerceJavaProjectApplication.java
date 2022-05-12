01ff38c6ce21e6c0cdfd9b5aa2251ae9fa9cd849
R.A.D.B.S.PERERA_IT18231656 <it18231656@my.sliit.lk>
2022-05-13 01:34:42 +0530

    Reformat and clean up.

--- src/main/java/lk/sliit/ecommercejavaproject/ECommerceJavaProjectApplication.java
+++ src/main/java/lk/sliit/ecommercejavaproject/ECommerceJavaProjectApplication.java
@@ -1,6 +1,5 @@
 package lk.sliit.ecommercejavaproject;
 
-import lk.sliit.ecommercejavaproject.config.LogConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -12,9 +11,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 })
 @EnableAspectJAutoProxy
 public class ECommerceJavaProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ECommerceJavaProjectApplication.class, args);
     }
-
+    
 }
